6f016ef0f73e6feb5cb8c5c4b9e89e8b040a42e3
LiMiaoJing <466020917@qq.com>
2017-08-02 11:48:43 +0800

    first alter

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ const index = require('./routes/index');
 
 let app = express();
 
+app.use(orm.express("sqlite:///Users/sanshui/Desktop/tw-movie-theater/SQL/movies.db", {
+    define: function (db, models, next) {
+        models.movie = db.define("movie", {
+          img:text,
+          title:text,
+          casts:text
+        });
+        next();
+    }
+}));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,6 +35,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
